c8390a439353fc6ce9541894e515bc9c5a992404
Laksh2104 <lakshmalhotra2004@gmail.com>
2021-05-05 01:07:16 +0530

    Upgraded

--- README.md
+++ README.md
@@ -6,4 +6,13 @@ Project is Live on:-
 https://indiafightscovid.netlify.app
 
 API used in this project:-
-https://corona.blloc.com
+
+1. https://corona.blloc.com
+2. https://coronasafe.network/
+
+Technologies Used:-
+
+1. HTML
+2. CSS
+3. SASS
+4. Javascript
--- app.js
+++ app.js
@@ -1,48 +1,75 @@
 "use strict";
 const container = document.querySelector("#container");
-// const ctx = document.getElementById("myChart").getContext("2d");
 
+// To make API call
 const getJSON = async function (url) {
   try {
     const res = await fetch(url);
 
     const data = await res.json();
 
-    if (!res.ok) throw Error("Data not found!!!");
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
     return data;
   } catch (err) {
-    console.log(err);
+    throw err;
   }
 };
 
+// To render error message
+const renderError = function (
+  message = "Data Not Found!!! Please Try Again..."
+) {
+  const html = `
+  <div class="error">
+    <p>${message}</p>
+  </div>
+  `;
+  container.innerHTML = "";
+  container.insertAdjacentHTML("beforeend", html);
+};
+
+// Getting data from API calls
+// data1 is of Total Country Cases and Statewise data
+// data2 is of Medicine Suppliers
+// data3 is of Oxygen Suppliers
+// data4 is of Ambulance Drivers
 const getData = async function () {
-  const data = await getJSON("https://corona.blloc.com/current?country=India");
+  try {
+    const data1 = await getJSON(
+      "https://corona.blloc.com/current?country=India"
+    );
+    const data2 = await getJSON(
+      "https://life-api.coronasafe.network/data/medicine_verified.json"
+    );
+    const data3 = await getJSON(
+      "https://life-api.coronasafe.network/data/oxygen_verified.json"
+    );
+    const data4 = await getJSON(
+      "https://life-api.coronasafe.network/data/ambulance_verified.json"
+    );
 
-  render(data);
-
-  // renderGraph(data);
+    render(data1, data2, data3, data4);
+  } catch (err) {
+    renderError();
+  }
 };
 getData();
 
-const render = function (data) {
+// Rendering data to user interface
+const render = function (data, data1, data2, data3) {
   const html = `
-  <h3 class="head">
-    COVID-19 CORONAVIRUS PANDEMIC
-      <span
-      ><a
-        target="_blank"
-        href="https://www.who.int/health-topics/coronavirus#tab=tab_1"
-        >Know More</a
-      ></span
-      >
-  </h3>
   <section id="info">
         <div class="stats">
           <h2 class="title">Coronavirus Cases:</h2>
           <h1 class="number-1 mb-1">${data.confirmed.toLocaleString(
             "en-IN"
           )}</h1>
-          <a class="btn" href="#stateview">View By States</a>
+          <div class="button">
+            <a class="btn btn-1" href="#stateview">State-Wise Status</a>
+            <a class="btn btn-2" href="#medicine">Medicine</a>
+            <a class="btn btn-3" href="#oxygen">Oxygen</a>
+            <a class="btn btn-4" href="#ambulance">Ambulance</a>
+          </div>
         </div>
         <div class="stats">
           <h2 class="title">Deaths:</h2>
@@ -72,6 +99,7 @@ const render = function (data) {
         </div>
       </section>
       <section id="stateview">
+      <h2>COVID-19 State-Wise Status</h2>
         <table>
           <tr>
             <th>State</th>
@@ -79,7 +107,43 @@ const render = function (data) {
             <th>Total Deaths</th>
             <th>Total Recovered</th>
           </tr>
-        ${data.statesData.map(table).join("")}
+        ${data.statesData.map(stateTable).join("")}
+        </table>
+      </section>
+      <section id="medicine">
+      <h2>Medicine</h2>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>State</th>
+            <th>Phone Number</th>
+            <th>Address</th>
+          </tr>
+        ${data1.data.map(mediTable).join("")}
+        </table>
+      </section>
+      <section id="oxygen">
+      <h2>Oxygen</h2>
+        <table>
+          <tr>
+            <th>State</th>
+            <th>District</th>
+            <th>Company Name</th>
+            <th>Name</th>
+            <th>Phone Number</th>
+          </tr>
+        ${data2.data.map(o2Table).join("")}
+        </table>
+      </section>
+      <section id="ambulance">
+      <h2>Ambulance</h2>
+        <table>
+          <tr>
+            <th>State</th>
+            <th>District</th>
+            <th>Phone Number</th>
+          </tr>
+        ${data3.data.map(vehicleTable).join("")}
         </table>
       </section>
   `;
@@ -87,7 +151,8 @@ const render = function (data) {
   container.style.opacity = 1;
 };
 
-const table = function (state) {
+// Statewise Data
+const stateTable = function (state) {
   return `
   <tr>
     <td>${state.label}</td>
@@ -98,26 +163,38 @@ const table = function (state) {
   `;
 };
 
-// const renderGraph = function (data) {
-//   const myChart = new Chart(ctx, {
-//     type: "doughnut",
-//     data: {
-//       labels: ["Total Cases", "Deaths", "Recovered"],
-//       color: ["#fff"],
-//       datasets: [
-//         {
-//           label: "# of Votes",
-//           data: [data.confirmed, data.deaths, data.recovered],
-//           backgroundColor: ["#fca311", "#d00000", "#00f5d4"],
-//         },
-//       ],
-//     },
-//     options: {
-//       legend: {
-//         labels: {
-//           fontColor: "white",
-//         },
-//       },
-//     },
-//   });
-// };
+// Medicine Suppliers
+const mediTable = function (info) {
+  return `
+  <tr>
+    <td>${info.state}</td>
+    <td>${info.district}</td>
+    <td>${info.phone1 ? info.phone1 : "Invailid"}</td>
+    <td>${info.address}</td>
+  </tr>
+  `;
+};
+
+// Oxygen Suppliers
+const o2Table = function (info) {
+  return `
+  <tr>
+    <td>${info.state}</td>
+    <td>${info.district}</td>
+    <td>${info.companyName ? info.companyName : "xyz"}</td>
+    <td>${info.name ? info.name : "xyz"}</td>
+    <td>${info.phone1 ? info.phone1 : "Invailid"}</td>
+  </tr>
+  `;
+};
+
+// Ambulance Drivers
+const vehicleTable = function (info) {
+  return `
+  <tr>
+    <td>${info.state}</td>
+    <td>${info.district}</td>
+    <td>${info.phone1 ? info.phone1 : "Invailid"}</td>
+  </tr>
+  `;
+};
