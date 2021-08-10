fbb38e70cb8d5d2d8874f7f23fb6b87a1b2bc765
Laksh2104 <lakshmalhotra2004@gmail.com>
2021-08-10 21:26:28 +0530

    Update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
 const container = document.querySelector("#container");
+let ctx;
 
 // To make API call
 const getJSON = async function (url) {
@@ -50,17 +51,8 @@ const getData = async function () {
     const data = await getJSON(
       "https://api.rootnet.in/covid19-in/stats/latest"
     );
-    const data1 = await getJSON(
-      "https://life-api.coronasafe.network/data/medicine_verified.json"
-    );
-    const data2 = await getJSON(
-      "https://life-api.coronasafe.network/data/oxygen_verified.json"
-    );
-    const data3 = await getJSON(
-      "https://life-api.coronasafe.network/data/ambulance_verified.json"
-    );
 
-    render(data, data1, data2, data3);
+    render(data);
   } catch (err) {
     renderError();
   }
@@ -68,7 +60,7 @@ const getData = async function () {
 getData();
 
 // Rendering data to user interface
-const render = function (data, data1, data2, data3) {
+const render = function (data) {
   const html = `
       <section id="info">
         <div class="stats">
@@ -76,12 +68,6 @@ const render = function (data, data1, data2, data3) {
           <h1 class="number-1 mb-1">${data.data.summary.total.toLocaleString(
             "en-IN"
           )}</h1>
-          <div class="button">
-            <a class="btn btn-1" href="#stateview">State-Wise Status</a>
-            <a class="btn btn-2" href="#medicine">Medicine</a>
-            <a class="btn btn-3" href="#oxygen">Oxygen</a>
-            <a class="btn btn-4" href="#ambulance">Ambulance</a>
-          </div>
         </div>
         <div class="stats">
           <h2 class="title">Deaths:</h2>
@@ -94,6 +80,9 @@ const render = function (data, data1, data2, data3) {
           <h1 class="number-3">${data.data.summary.discharged.toLocaleString(
             "en-IN"
           )}</h1>
+        </div>
+        <div class="chart">
+          <canvas id="myChart"></canvas>
         </div>
       </section>
       <section id="info-1">
@@ -126,46 +115,12 @@ const render = function (data, data1, data2, data3) {
         ${data.data.regional.map(stateTable).join("")}
         </table>
       </section>
-      <section id="medicine">
-      <h2>Medicine</h2>
-        <table>
-          <tr>
-            <th>Name</th>
-            <th>State</th>
-            <th>Phone Number</th>
-            <th>Address</th>
-          </tr>
-        ${data1.data.map(mediTable).join("")}
-        </table>
-      </section>
-      <section id="oxygen">
-      <h2>Oxygen</h2>
-        <table>
-          <tr>
-            <th>State</th>
-            <th>District</th>
-            <th>Company Name</th>
-            <th>Name</th>
-            <th>Phone Number</th>
-          </tr>
-        ${data2.data.map(o2Table).join("")}
-        </table>
-      </section>
-      <section id="ambulance">
-      <h2>Ambulance</h2>
-        <table>
-          <tr>
-            <th>State</th>
-            <th>District</th>
-            <th>Phone Number</th>
-          </tr>
-        ${data3.data.map(vehicleTable).join("")}
-        </table>
-      </section>
   `;
   container.innerHTML = "";
   container.insertAdjacentHTML("beforeend", html);
   container.style.opacity = 1;
+  ctx = document.getElementById("myChart").getContext("2d");
+  renderChart(data);
 };
 
 // Statewise Data
@@ -180,38 +135,31 @@ const stateTable = function (state) {
   `;
 };
 
-// Medicine Suppliers
-const mediTable = function (info) {
-  return `
-  <tr>
-    <td>${info.state}</td>
-    <td>${info.district}</td>
-    <td>${info.phone1 ? info.phone1 : "Invailid"}</td>
-    <td>${info.address}</td>
-  </tr>
-  `;
-};
-
-// Oxygen Suppliers
-const o2Table = function (info) {
-  return `
-  <tr>
-    <td>${info.state}</td>
-    <td>${info.district}</td>
-    <td>${info.companyName ? info.companyName : "xyz"}</td>
-    <td>${info.name ? info.name : "xyz"}</td>
-    <td>${info.phone1 ? info.phone1 : "Invailid"}</td>
-  </tr>
-  `;
-};
-
-// Ambulance Drivers
-const vehicleTable = function (info) {
-  return `
-  <tr>
-    <td>${info.state}</td>
-    <td>${info.district}</td>
-    <td>${info.phone1 ? info.phone1 : "Invailid"}</td>
-  </tr>
-  `;
+// To render pie chart
+const renderChart = function (data) {
+  const myChart = new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: ["Total Cases", "Recovered", "Deaths"],
+      datasets: [
+        {
+          backgroundColor: ["#fca311", "#00f5d4", "#d00000"],
+          data: [
+            data.data.summary.total,
+            data.data.summary.discharged,
+            data.data.summary.deaths,
+          ],
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          labels: {
+            color: "#ffffff",
+          },
+        },
+      },
+    },
+  });
 };
